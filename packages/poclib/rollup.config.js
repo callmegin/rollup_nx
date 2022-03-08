42685f0718775079d35dff2327abfc754fcf2896
gintautas.anduzis <gintautas.anduzis@sapiens.com>
2022-03-08 16:45:06 +0200

    manually added plugins used by default rollup config

--- packages/poclib/rollup.config.js
+++ packages/poclib/rollup.config.js
@@ -1,71 +1,25 @@
-// import typescript from 'rollup-plugin-typescript2';
-
-// export default {
-//   input: 'aps/wtf2/main.ts',
-//   output: {
-//     dir: 'DAbunDLE',
-//     format: 'umd',
-//   },
-//   plugins: [typescript()],
-// };
 const replace = require('@rollup/plugin-replace');
+const rpt2 = require('rollup-plugin-typescript2');
+const externalPeerDeps = require('rollup-plugin-peer-deps-external');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const { babel } = require('@rollup/plugin-babel');
+const commonjs = require('@rollup/plugin-commonjs');
+
 module.exports = (config) => {
-  // const typescript = require('rollup-plugin-typescript2');
-  // const commonjs = require('@rollup/plugin-commonjs');
-  // const resolve = require('@rollup/plugin-node-resolve');
-
-  // const { DEFAULT_EXTENSIONS } = require('@babel/core');
-  // const babel = require('@rollup/plugin-babel');
-  // const peerDepsExternal = require('rollup-plugin-peer-deps-external');
-  // const nrwlConfig = require('@nrwl/react/plugins/bundle-rollup');
-
-  // const nxConfig = nrwlConfig(config);
-
-  console.log('incoming', config);
-  // let tstConfig = { ...config };
-
-  // tstConfig = {
-  //   plugins: [
-  //     babel.babel({
-  //       babelHelpers: 'runtime',
-  //       presets: [['@babel/preset-typescript']],
-  //       skipPreflightCheck: true,
-  //       extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
-  //     }),
-  //   ],
-  // };
-  // console.log('tstConfig', tstConfig);
   const myConfig = {
     ...config,
-    // input:
-    //   'C:\\Users\\gintautas.anduzis\\WORK\\TEST\\trying-nx/libs/lib-app/src/index.ts',
-    // output: {
-    //   ...config.output,
-    //   plugins: [
-    //     replace({
-    //       preventAssignment: true,
-    //       "process.env.NODE_ENV": JSON.stringify("production"),
-    //     }),
-    //   ],
-    // },
-    // plugins: [
-    //   // peerDepsExternal({
-    //   //   packageJsonPath: '../../package.json',
-    //   // }),
-    //   // typescript(),
-    //   // babel.babel({
-    //   //   babelHelpers: 'bundled',
-    //   //   presets: [['@babel/preset-typescript']],
-    //   //   skipPreflightCheck: true,
-    //   //   extensions: [...DEFAULT_EXTENSIONS, '**/*.ts', '**/*.tsx'],
-    //   // }),
-    //   // commonjs(),
-    //   // resolve.nodeResolve(),
-
-    //   ...nxConfig.plugins,
-    // ],
+    plugins: [
+      rpt2(),
+      externalPeerDeps(),
+      nodeResolve(),
+      babel({
+        babelHelpers: 'bundled',
+        // presets: [['@babel/preset-react']],
+      }),
+      commonjs(),
+    ],
   };
   // console.log('==================================');
-  // console.log('my config', myConfig);
+  console.log('my config', myConfig);
   return myConfig;
 };
